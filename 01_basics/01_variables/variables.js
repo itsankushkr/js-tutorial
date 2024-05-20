502e6973184c72cdfdce069834cd5f04b5bb7418
Ankush Kumar <itsankushkr@gmail.com>
2024-05-20 10:49:07 +0000

    variables basics

---
+++ 01_basics/01_variables/variables.js
@@ -0,0 +1,18 @@
+const userId = "123"
+let userName = "Ankush"
+let userPwd = "Test"
+var userAddress = "Gurugram"
+userEmail = "ankush@gmail.com"
+
+console.table([userId, userName, userPwd, userAddress, userEmail])
+
+userPwd = "NewTest"
+userEmail = "itsankushkr@gmail.com"
+
+console.table([userId, userName, userPwd, userAddress, userEmail])
+
+//Assignment to const not allowed
+//userId = "111"
+
+//const variables must be initialized
+//const testVar
