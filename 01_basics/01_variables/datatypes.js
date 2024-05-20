9e06860b2650df68c4f41974115e2965e2d7d333
Ankush Kumar <itsankushkr@gmail.com>
2024-05-20 11:09:13 +0000

    datatypes basics

---
+++ 01_basics/01_variables/datatypes.js
@@ -0,0 +1,22 @@
+//Primitive data types
+//number
+//bigint
+//boolean
+//String
+//null
+//undefined
+//symbol
+
+let userName = "Ankush"
+let age = 27
+let salary = 122222222222222222222222222222222n
+let isMarried = true
+let criminalCases = null
+let kids
+
+console.log(typeof userName)
+console.log(typeof salary)
+console.log(typeof criminalCases) // type of null is object
+console.log(typeof kids) //type of undefined is undefined
+
+//object
